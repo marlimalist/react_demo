d5765978593f27f48095bf7490b21577d7faa2d2
marlimalist <liske.marco@gmail.com>
2023-06-30 20:15:52 +0200

    Uebung einfacher Umrechner

--- src/ExampleConverter.tsx
+++ src/ExampleConverter.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import "./App.css";
 
 export function Converter() {
   const [name, setName] = useState("");
@@ -15,42 +16,36 @@ export function Converter() {
     <React.Fragment>
       {" "}
       <div className="Converter">
-        <div>
-          <label>Crypto-Name:</label>
-          <input
-            type="text"
-            placeholder="Name"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          ></input>
-        </div>
-        <div>
-          <label>aktueller Wechselkurs:</label>
-          <input
-            type="number"
-            placeholder="Wechselkurs"
-            value={exchange}
-            onChange={(e) => setExchange(parseFloat(e.target.value))}
-          ></input>
-        </div>
-        <div>
-          <label>Anzahl Coins:</label>
-          <input
-            type="number"
-            placeholder="Anzahl"
-            value={coins}
-            onChange={(e) => setCoins(parseFloat(e.target.value))}
-          ></input>
-        </div>
-        <div>
+        <label>Crypto-Name:</label>
+        <input
+          type="text"
+          placeholder="Name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        ></input>
+
+        <label>aktueller Wechselkurs:</label>
+        <input
+          type="number"
+          placeholder="Wechselkurs"
+          value={exchange}
+          onChange={(e) => setExchange(parseFloat(e.target.value))}
+        ></input>
+
+        <label>Anzahl Coins:</label>
+        <input
+          type="number"
+          placeholder="Anzahl"
+          value={coins}
+          onChange={(e) => setCoins(parseFloat(e.target.value))}
+        ></input>
+        <div className="actionButton">
           <button onClick={() => convert()}>Berechne</button>
         </div>
-        <div>
-          <p>
-            Mit einem Wechselkurs von {exchange} und der Anzahl von {coins}{" "}
-            {name} hast du {result}€{" "}
-          </p>
-        </div>
+        <p>
+          Mit einem Wechselkurs von {exchange} und der Anzahl von {coins} {name}{" "}
+          hast du {result}€{" "}
+        </p>
       </div>
     </React.Fragment>
   );
